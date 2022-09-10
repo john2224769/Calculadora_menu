d949cd6810e06cbae40a47db4c168ea9fd2b191d
john2224769 <john2224769@correo.uis.edu.co>
2022-09-10 08:50:06 -0500

    Codigo final

--- Calculadora_menu.py
+++ Calculadora_menu.py
@@ -1,5 +1,5 @@
 #CALCULADORA BÁSICA CON MENU
-import math
+from math import log
 
 #despliegue de menu
 print(" --------------------------------------------------------------------------------")
@@ -13,7 +13,7 @@ print("Ingrese 6 para logaritmo Log X ('Y') \n")
 print(" ---------------------------------------------------------------------------------")
 
 #input
-bandrea=False
+bandera=False
 n=int(input("\nIngrese la opcion a realizar: "))
 x=float(input("\nIngrese el primer numero 'x': "))
 y=float(input("\nIngrese el segundo numero 'Y': "))
@@ -21,32 +21,31 @@ y=float(input("\nIngrese el segundo numero 'Y': "))
 #processing
 if n==1:
     r=x+y
-    print("\nEl resultado de la suma entre "+str(x)+" y "+str(y)+" es: "+str(r)+"\n")
+    print("\nEl resultado de la suma "+str(x)+"+"+str(y)+" es: ")
+elif n==2:
+    r=x-y
+    print("\nEl resultado de la resta entre "+str(x)+"-"+str(y)+" es: ")
+elif n==3:
+    r=x*y
+    print("\nEl resultado de la multiplicacion "+str(x)+"*"+str(y)+" es: ")
+elif n==4 and y!=0:
+    r=x/y
+    print("\nEl resultado de la division entre "+str(x)+"/"+str(y)+" es: ")
+elif n==4 and y==0:
+    print("\nLa division no se puede realizar, el denominador es igual a cero\n")
+    bandera=True 
+elif n==5:
+    r=pow(x,y)
+    print("\nEl resultado de la potencia entre "+str(x)+"^"+str(y)+" es: ")
+elif n==6 and x>1:
+    r=log(y)/log(x)
+    print("\nEl resultado del logaritmo base "+str(x)+" de "+str(y)+" es: ")
+elif n==6 and x<=1:
+    print("\nLa base ingresada es menor o igual a uno, no es posible calcular el logaritmo\n")
+    bandera=True
 else:
-    if n==2:
-        r=x-y
-        print("\nEl resultado de la resta entre "+str(x)+" y "+str(y)+" es: "+str(r)+"\n")
-    else:
-        if n==3:
-            r=x*y
-            print("\nEl resultado de la multiplicacion entre "+str(x)+" y "+str(y)+" es: "+str(r)+"\n")
-        else:
-            if n==4:
-                if y!=0:
-                    r=x/y
-                    print("\nEl resultado de la division entre "+str(x)+" y "+str(y)+" es: "+str(r)+"\n")
-                else:
-                    print("\nLa division no se puede realizar, el denominador es igual a cero\n")
-            else:
-                if n==5:
-                    r=x**y
-                    print("\nEl resultado de la potencia entre "+str(x)+" y "+str(y)+" es: "+str(r)+"\n")
-                else:
-                    if n==6:
-                        if x>1:
-                            r=math.log(y)/math.log(x)
-                            print("\nEl resultado del logaritmo base "+str(x)+" y "+str(y)+" es: "+str(r)+"\n")
-                        else:
-                            print("\nLa base ingresada es menor a cero, no es posible calcular el logaritmo\n")
-                    else:
-                        print("\nLa opcion ingresada no esta en el menu o es incorrecta\n")
+    print("\nLa opcion ingresada no esta en el menu o es incorrecta\n")
+
+if n<7 and bandera==False:
+    print(r)
+    print(" ")
